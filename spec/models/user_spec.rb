f6dedfa94a8e90db08d4be640d4932c07104874b
Randal Humphries <randal@randalhumphries.com>
2019-03-16 09:24:57 -0400

    Updates 'user' model spec
    Why:
    
    * Test the initial state of the 'User' model
    
    This change addresses the need by:
    
    * Updating the 'user' factory
    * Updating the 'user' model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'is valid with valid attributes' do
+    expect(build(:user)).to be_valid
+  end
+
+  it 'is not valid without a valid email attribute' do
+    expect(build(:user, email: nil)).to_not be_valid
+  end
+
+  it 'is not valid without a valid password attribute' do
+    expect(build(:user, password: nil)).to_not be_valid
+  end
+
+  it 'is not valid without a valid company attribute' do
+    expect(build(:user, company: nil)).to_not be_valid
+  end
 end
