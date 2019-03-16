497f25ec64c3c7c75f775838eaecaf4415974124
Randal Humphries <randal@randalhumphries.com>
2019-03-16 09:10:13 -0400

    Updates 'company' model spec
    Why:
    
    * Test initial state of the 'company' model
    
    This change addresses the need by:
    
    * Updating the 'companies' factory
    * Updating the 'company' spec

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it 'is valid with valid attributes' do
+    expect(build(:company)).to be_valid
+  end
+
 end
