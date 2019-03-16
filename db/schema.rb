ed35b61048c31219febb0120ac5f16aad3a087bc
Randal Humphries <randal@randalhumphries.com>
2019-03-16 09:08:09 -0400

    Updates 'User' model
    Why:
    
    * Associate the 'User' and 'Company' models
    
    This change addresses the need by:
    
    * Generating a migration to add the 'first_name', 'last_name', and
      'company' columns to the 'User' model
    * Adding `has_many` association to the 'Company' and 'User' models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_16_124311) do
+ActiveRecord::Schema.define(version: 2019_03_16_124507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,13 @@ ActiveRecord::Schema.define(version: 2019_03_16_124311) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.bigint "company_id"
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "companies"
 end
