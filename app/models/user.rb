c5e68d65077e1d431d657af9d3a573fcb4b21a35
Randal Humphries <randal@randalhumphries.com>
2019-03-16 14:46:00 -0400

    Updates 'User' model
    Why:
    
    * Allow company to be created via user model
    
    This change addresses the need by:
    
    * Adding `accepts_nested_attributes_for` method to 'User' model
    * Adding the `configure_permitted_parameters`
      method to 'Application' controller
    * Adding fields for the user's first name, last name, and company name
      to the 'new' devise view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   belongs_to :company
-
+  accepts_nested_attributes_for :company
   validates :first_name, :last_name, presence: true
 
   # Include default devise modules. Others available are:
