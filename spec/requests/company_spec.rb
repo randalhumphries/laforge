2f6415d68cfc33bcc8be6b854ee03ec7be5c648e
Randal Humphries <randal@randalhumphries.com>
2019-03-16 13:33:50 -0400

    Updates 'Companies' request spec
    Why:
    
    * Refactor request spec
    
    This change addresses the need by:
    
    * Updating the 'Companies' request spec

--- spec/requests/company_spec.rb
+++ spec/requests/company_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "CompanyController", type: :request do
-  it 'redirects to the login page when user is not authenticated' do
-    get "/"
-    expect(response).to redirect_to("/users/sign_in")
+RSpec.describe "Companies", type: :request do
+  describe 'request list of all companies' do
+    it 'redirects to the login page when user is not authenticated' do
+      get root_path
+      expect(response).to redirect_to(new_user_session_path)
+    end
   end
 end
