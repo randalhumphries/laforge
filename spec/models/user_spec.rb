854f0e44174819279843b3e303e60951d1d79231
Randal Humphries <randal@randalhumphries.com>
2019-03-16 09:58:28 -0400

    Updates 'user' model
    Why:
    
    * Provide method to return the user's full name (first name last name)
    
    This change addresses the need by:
    
    * Adding the `full_name` method to the 'User' model
    * Updating the 'user' model spec for the `full_name` method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
   it 'is valid with valid attributes' do
     expect(build(:user)).to be_valid
   end
@@ -24,5 +23,14 @@ RSpec.describe User, type: :model do
 
   it 'is not valid without a valid last_name attribute' do
     expect(build(:user, last_name: nil)).to_not be_valid
+  end
+
+  describe '.full_name' do
+    it 'should return the first_name and last_name attributes as a concatenated string' do
+      @user           = build(:user)
+      @user_full_name = @user.first_name + ' ' + @user.last_name
+
+      expect(@user.full_name).to eq(@user_full_name)
+    end
   end
 end
