f416a88f8fe6a8478647389639b2d21899760229
Randal Humphries <randal@randalhumphries.com>
2019-03-16 12:10:30 -0400

    Updates 'Company' controller
    Why:
    
    * Create 'Company' controller actions
    
    This change addresses the need by:
    
    * Adding the 'index', 'new', 'show', 'edit', 'update', and 'destroy'
      actions to the 'Company' controller
    * Adding the 'company_params', 'find_companies', and 'find_company'
      methods to the 'Company' controller
    * Adding the `before_action` methods to the 'Company' controller

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -1,22 +1,52 @@
 class CompanyController < ApplicationController
+  before_action :authenticate_user
+  before_action :find_companies, only: %i[index]
+  before_action :find_company, except: %i[index create new]
+  
   def index
   end
 
   def new
+    @company = Company.new
   end
 
   def create
+    @company = Company.create(company_params)
+
+    if @company.save
+      render :show
+    end
+  end
+  
+  def show
   end
 
   def edit
   end
 
-  def show
-  end
 
   def update
+    if @company.update(company_params)
+      render :show
+    end
   end
 
   def destroy
+    if @company.destroy
+      render :index
+    end
   end
+
+  private
+    def company_params
+      params.require(:company).permit(:name)
+    end
+
+    def find_company
+      @company = Company.find(params[:id])
+    end
+
+    def find_companies
+      @companies = Company.all
+    end
 end
