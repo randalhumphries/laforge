84be800ed83333ca7382e3a5c3e00d4f572e3590
Randal Humphries <randal@randalhumphries.com>
2019-03-16 14:22:32 -0400

    Revert "Creates 'Registrations' controller"
    This reverts commit 7e40a1118917cb9afa818465aa654c6eeb7c9c29.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   root 'company#index'
 
   resources :company
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
