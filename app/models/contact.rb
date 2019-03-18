f97310dad805b7542d33d387d291c9e9cafb7a42
Randal Humphries <randal@randalhumphries.com>
2019-03-17 21:40:43 -0400

    Updates 'Contact' model
    Why:
    
    * Provide ability to display a contact's first and last names
    
    This change addresses the need by:
    
    * Adding the `full_name` method to the 'Contact' model
    * Updating the 'Contact' model spec

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,8 @@ class Contact < ApplicationRecord
   belongs_to :client
   
   validates :first_name, :last_name, :email, :primary_phone, presence: true
+
+  def full_name
+    first_name + ' ' + last_name
+  end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -20,4 +20,13 @@ RSpec.describe Contact, type: :model do
   it 'is not valid without a primary phone number attribute' do
     expect(build(:contact, primary_phone: nil)).to_not be_valid
   end
+
+  describe '.full_name' do
+    it 'should return the first_name and last_name attributes as a concatenated string' do
+      @contact           = build(:contact)
+      @contact_full_name = @contact.first_name + ' ' + @contact.last_name
+
+      expect(@contact.full_name).to eq(@contact_full_name)
+    end
+  end
 end
