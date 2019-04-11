7fff6fda44e3f721acb414db680161c7aedd6820
Randal Humphries <randal@randalhumphries.com>
2019-04-10 20:26:53 -0400

    Adds 'Accounts' controller views
    Why:
    
    * Provide way to create, view, edit, and destroy accounts
    
    This change addresses the need by:
    
    * Creating the 'new', 'edit', and 'form' partials
    * Creating the 'new' and 'edit' JavaScript views
    * Updating the 'show' view of the 'Clients' controller for the
      'Accounts' views

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_013015) do
+ActiveRecord::Schema.define(version: 2019_04_11_001701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
-    t.string "type"
+    t.string "account_type"
     t.string "address"
     t.string "username"
     t.string "password"
     t.bigint "client_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["client_id"], name: "index_accounts_on_client_id"
   end
 
