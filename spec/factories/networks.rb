6b1fad92b3127d90da0090337ae671b5d2dea5f2
Randal Humphries <randal@randalhumphries.com>
2019-04-13 17:07:23 -0400

    Updates 'network' model spec
    Why:
    
    * Provide testing for the 'Network' model
    
    This change addresses the need by:
    
    * Updating the 'Network' model spec
    * Updating the 'networks' factory
    * Updating the 'networks' request spec

--- spec/factories/networks.rb
+++ spec/factories/networks.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :network do
-    network_type { "MyString" }
-    ip_subnet { "MyString" }
-    description { "MyString" }
-    client { nil }
+    network_type { ["Range", "Subnet", "VLAN", "Gateway", "Host"].sample }
+    ip_subnet    { Faker::Internet.private_ip_v4_address }
+    description  { Faker::Lorem.sentence }
+    client
   end
 end
--- spec/models/network_spec.rb
+++ spec/models/network_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Network, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "is valid with valid attributes" do
+    expect(build(:network)).to be_valid
+  end
+
+  it "is not valid without a network type" do
+    expect(build(:network, network_type: nil)).to_not be_valid
+  end
+
+  it "is not valid without an IP address or subnet" do
+    expect(build(:network, ip_subnet: nil)).to_not be_valid
+  end
 end
